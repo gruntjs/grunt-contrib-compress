7935b3056fad5bd1b37917b8eb2c37f33da219bb
Chris Talkington <chris@christalkington.com>
2012-09-10 15:24:54 -0500

    remove debug logging in last commit.

--- tasks/compress.js
+++ tasks/compress.js
@@ -54,7 +54,6 @@ module.exports = function(grunt) {
 
     if (options.rootDir && kindOf(options.rootDir) === "string") {
       options.rootDir = path.normalize(options.rootDir).split(path.sep)[0];
-      console.log(options.rootDir);
     } else {
       options.rootDir = false;
     }
