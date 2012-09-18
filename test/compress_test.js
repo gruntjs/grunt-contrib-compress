29e119b47669c8b1b67d22a8c36f71212a28ac09
Chris Talkington <chris@christalkington.com>
2012-09-18 02:27:33 -0500

    test refactoring.
    still need a better way to check gzip as its size varies by OS but this
    is a start.

--- test/compress_test.js
+++ test/compress_test.js
@@ -1,44 +1,55 @@
 var grunt = require('grunt');
-var path = require('path');
 var fs = require('fs');
 
 var getSize = function(filename) {
   'use strict';
 
   try {
-    return fs.statSync(path.join('tmp', filename)).size;
+    return fs.statSync(filename).size;
   } catch (e) {
     return 0;
   }
 };
 
-exports['compress'] = {
+exports.compress = {
   zip: function(test) {
     'use strict';
 
-    var expect, result;
+    test.expect(3);
 
-    test.expect(5);
+    var actual = getSize('tmp/compress_test_files.zip');
+    var expected = getSize('test/expected/compress_test_files.zip');
+    test.equal(expected, actual, 'should compress files into zip');
 
-    expect = 310;
-    result = getSize('compress_test_files.zip');
-    test.equal(expect, result, 'should compress files into zip');
+    actual = getSize('tmp/compress_test_v0.1.0.zip');
+    expected = getSize('test/expected/compress_test_v0.1.0.zip');
+    test.equal(expected, actual, 'should compress folders and their files into zip (with template support)');
 
-    expect = 962;
-    result = getSize('compress_test_v0.1.0.zip');
-    test.equal(expect, result, 'should compress folders and their files into zip (with template support)');
+    actual = getSize('tmp/compress_test_files_template.zip');
+    expected = getSize('test/expected/compress_test_files_template.zip');
+    test.equal(expected, actual, 'should compress files and folders into zip (grunt template in source)');
 
-    expect = 302;
-    result = getSize('compress_test_files_template.zip');
-    test.equal(expect, result, 'should compress files and folders into zip (grunt template in source)');
+    test.done();
+  },
+  zip_flatten: function(test) {
+    'use strict';
 
-    expect = 874;
-    result = getSize('compress_test_flatten.zip');
-    test.equal(expect, result, 'should create a flat internal structure');
+    test.expect(1);
 
-    expect = 1046;
-    result = getSize('compress_test_rootdir.zip');
-    test.equal(expect, result, 'should compress folders and their files into zip (with a root dir)');
+    var actual = getSize('tmp/compress_test_flatten.zip');
+    var expected = getSize('test/expected/compress_test_flatten.zip');
+    test.equal(expected, actual, 'should create a flat internal structure');
+
+    test.done();
+  },
+  zip_rootdir: function(test) {
+    'use strict';
+
+    test.expect(1);
+
+    var actual = getSize('tmp/compress_test_rootdir.zip');
+    var expected = getSize('test/expected/compress_test_rootdir.zip');
+    test.equal(expected, actual, 'should compress folders and their files into zip (with a root dir)');
 
     test.done();
   },
@@ -46,29 +57,41 @@ exports['compress'] = {
   tar: function(test) {
     'use strict';
 
-    var expect, result;
+    test.expect(3);
 
-    test.expect(5);
+    var actual = getSize('tmp/compress_test_files.tar');
+    var expected = getSize('test/expected/compress_test_files.tar');
+    test.equal(expected, actual, 'should add files into tar');
 
-    expect = 3072;
-    result = getSize('compress_test_files.tar');
-    test.equal(expect, result, 'should add files into tar');
+    actual = getSize('tmp/compress_test_v0.1.0.tar');
+    expected = getSize('test/expected/compress_test_v0.1.0.tar');
+    test.equal(expected, actual, 'should add folders and their files into tar (with template support)');
 
-    expect = 8192;
-    result = getSize('compress_test_v0.1.0.tar');
-    test.equal(expect, result, 'should add folders and their files into tar (with template support)');
+    actual = getSize('tmp/compress_test_files_template.tar');
+    expected = getSize('test/expected/compress_test_files_template.tar');
+    test.equal(expected, actual, 'should add files and folders into tar (grunt template in source)');
 
-    expect = 3072;
-    result = getSize('compress_test_files_template.tar');
-    test.equal(expect, result, 'should add files and folders into tar (grunt template in source)');
+    test.done();
+  },
+  tar_flatten: function(test) {
+    'use strict';
 
-    expect = 7168;
-    result = getSize('compress_test_flatten.tar');
-    test.equal(expect, result, 'should create a flat internal structure');
+    test.expect(1);
 
-    expect = 8192;
-    result = getSize('compress_test_rootdir.tar');
-    test.equal(expect, result, 'should compress folders and their files into tar (with a custom root dir)');
+    var actual = getSize('tmp/compress_test_flatten.tar');
+    var expected = getSize('test/expected/compress_test_flatten.tar');
+    test.equal(expected, actual, 'should create a flat internal structure');
+
+    test.done();
+  },
+  tar_rootdir: function(test) {
+    'use strict';
+
+    test.expect(1);
+
+    var actual = getSize('tmp/compress_test_rootdir.tar');
+    var expected = getSize('test/expected/compress_test_rootdir.tar');
+    test.equal(expected, actual, 'should compress folders and their files into tar (with a custom root dir)');
 
     test.done();
   },
@@ -76,29 +99,41 @@ exports['compress'] = {
   tgz: function(test) {
     'use strict';
 
-    var expect, result;
+    test.expect(3);
 
-    test.expect(5);
+    var actual = getSize('tmp/compress_test_files.tgz') >= 200;
+    var expected = true;
+    test.equal(expected, actual, 'should compress files into tar');
 
-    expect = true;
-    result = getSize('compress_test_files.tgz') >= 200;
-    test.equal(expect, result, 'should compress files into tar');
+    actual = getSize('tmp/compress_test_v0.1.0.tgz') >= 350;
+    expected = true;
+    test.equal(expected, actual, 'should compress folders and their files into tgz (with template support)');
 
-    expect = true;
-    result = getSize('compress_test_v0.1.0.tgz') >= 350;
-    test.equal(expect, result, 'should compress folders and their files into tgz (with template support)');
+    actual = getSize('tmp/compress_test_files_template.tgz') >= 200;
+    expected = true;
+    test.equal(expected, actual, 'should compress files and folders into tgz (grunt template in source)');
 
-    expect = true;
-    result = getSize('compress_test_files_template.tgz') >= 200;
-    test.equal(expect, result, 'should compress files and folders into tgz (grunt template in source)');
+    test.done();
+  },
+  tgz_flatten: function(test) {
+    'use strict';
 
-    expect = true;
-    result = getSize('compress_test_flatten.tgz') >= 320;
-    test.equal(expect, result, 'should create a flat internal structure');
+    test.expect(1);
 
-    expect = true;
-    result = getSize('compress_test_rootdir.tgz') >= 200;
-    test.equal(expect, result, 'should compress folders and their files into tgz (with a custom root dir)');
+    var actual = getSize('tmp/compress_test_flatten.tgz') >= 320;
+    var expected = true;
+    test.equal(expected, actual, 'should create a flat internal structure');
+
+    test.done();
+  },
+  tgz_rootdir: function(test) {
+    'use strict';
+
+    test.expect(1);
+
+    var actual = getSize('tmp/compress_test_rootdir.tgz') >= 200;
+    var expected = true;
+    test.equal(expected, actual, 'should compress folders and their files into tgz (with a custom root dir)');
 
     test.done();
   },
@@ -106,17 +141,15 @@ exports['compress'] = {
   gzip: function(test) {
     'use strict';
 
-    var expect, result;
-
     test.expect(2);
 
-    expect = 52;
-    result = getSize('compress_test_file.gz');
-    test.equal(expect, result, 'should gzip file');
+    var actual = getSize('tmp/compress_test_file.gz');
+    var expected = 52;
+    test.equal(expected, actual, 'should gzip file');
 
-    expect = 67;
-    result = getSize('compress_test_file2.gz');
-    test.equal(expect, result, 'should gzip another file (multiple dest:source pairs)');
+    actual = getSize('tmp/compress_test_file2.gz');
+    expected = 67;
+    test.equal(expected, actual, 'should gzip another file (multiple dest:source pairs)');
 
     test.done();
   }
