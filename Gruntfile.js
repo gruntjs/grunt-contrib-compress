ea88ade76139aebc49f0ea130fd48aaad312bc71
Richard Marr <richard.marr@gmail.com>
2013-04-23 15:28:26 +0100

    Added deflate and deflateRaw support. Closes GH-44.

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,6 +61,24 @@ module.exports = function(grunt) {
         options: {
           mode: 'gzip'
         }
+      },
+      deflate: {
+        expand: true,
+        cwd: 'test/fixtures/',
+        src: ['**/*.{css,html,js}'],
+        dest: 'tmp/deflate/',
+        options: {
+          mode: 'deflate'
+        }
+      },
+      deflateRaw: {
+        expand: true,
+        cwd: 'test/fixtures/',
+        src: ['**/*.{css,html,js}'],
+        dest: 'tmp/deflateRaw/',
+        options: {
+          mode: 'deflateRaw'
+        }
       }
     },
 
